0e80ac5e643031e54dee026529e0e7d92812baef
Tolfx <matteus12122@gmail.com>
2021-01-22 13:02:11 +0100

    Tables model new

---
+++ models/Tables.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const Table = new mongoose.Schema({
+    tableName: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Tables = mongoose.model("table", Table);
+
+const TableData = new mongoose.Schema({
+    tableData: {
+        type: Array,
+        required: true
+    },
+    tableConnectId: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const TablesData = mongoose.model("tabledata", TableData);
+
+module.exports = { Tables, TablesData };
