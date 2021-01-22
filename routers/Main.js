95f4458c379108ca9a05ff3a95d13cd69005e5d2
Tolfx <matteus12122@gmail.com>
2021-01-22 11:42:58 +0100

    Added setup

--- routers/Main.js
+++ routers/Main.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
 const User = require('../models/User');
+const log = require("../lib/Loggers");
+const { checkSetup, ensureIsLoggedIn } = require("../configs/Authenticate");
 
-router.get("/", (req, res) => {
+router.get("/", checkSetup, ensureIsLoggedIn, (req, res) => {
   res.render("home");
 });
 
@@ -13,7 +15,7 @@ router.get("/setup", (req, res) => {
     if(u) {
       return res.redirect("back");
     } else {
-      res.render("setup");
+      res.render("partials/setup");
     }
   });
 });
@@ -23,27 +25,61 @@ router.post("/setup", (req, res) => {
     if(u) {
       return res.redirect("back");
     } else {
-      //Todo
+      let { password, password2, email } = req.body;
+
+      if(!email) {
+        req.flash("error_msg", "No email found");
+        return res.redirect("back");
+      }
+
+      if(password != password2) {
+        req.flash("error_msg", "The password didn't match");
+        return res.redirect("back");
+      } else {
+        try {
+          let hashPassword = password
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(password, salt, (err, hash) => {
+              if(err)
+                log.error(err)
+
+                new User({
+                  username: "admin",
+                  password: hash,
+                  email: email,
+                  role: "admin"
+                }).save().then(a => {
+                  log.debug("Admin user created for setup");
+                  req.flash("succes_msg", "Admin user has been created!")
+                  res.redirect("/login");
+                })
+              })
+            })
+            
+        } catch(err) {
+          log.error(err)
+        }
+      }
     }
   });
 });
 
 //Login route
-router.get('/login', (req, res) => {
+router.get('/login', checkSetup, (req, res) => {
   res.render('login');
 });
 
 // Login
-router.post('/login', (req, res, next) => {
+router.post('/login', checkSetup, (req, res, next) => {
   passport.authenticate('local', {
-    successRedirect: '/lobby',
+    successRedirect: '/',
     failureRedirect: '/login',
     failureFlash: true,
   })(req, res, next);
 });
 
 // Logout
-router.get('/logout', (req, res) => {
+router.get('/logout', checkSetup, (req, res) => {
   req.logout();
   req.flash('success_msg', 'Logged out');
   res.redirect('/login');
