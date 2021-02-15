5e9b053ad43df7c5ee0ee0fcd0802112af7e6ada
Tolfx <matteus12122@gmail.com>
2021-02-15 18:24:54 +0100

    Modified user schema

--- models/User.js
+++ models/User.js
@@ -1,22 +1,27 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
+
   username: {
     type: String,
     required: true,
   },
+
   email: {
     type: String,
     required: true,
   },
+
   password: {
     type: String,
     required: true,
   },
+
   role: {
     type: String,
     default: 'member',
   },
+
   joined: {
     type: Date,
     default: Date.now,
