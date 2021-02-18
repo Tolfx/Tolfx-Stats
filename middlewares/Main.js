717ca1edc2eeaaacbc65fe50c774f32dd1ea52e2
Tolfx <matteus12122@gmail.com>
2021-02-18 17:35:33 +0100

    Added new middlewares

--- middlewares/Main.js
+++ middlewares/Main.js
@@ -1,7 +1,17 @@
-let CheckSetup = require("./CheckSetup");
-let SetGeneral = require("./SetGeneral");
+const CheckSetup = require("./CheckSetup");
+const SetGeneral = require("./SetGeneral");
+const Pagination = require("./Pagination");
+const CanRemove = require("./Permissions").canRemove
+const CanCreate = require("./Permissions").canCreate
+const CanWrite = require("./Permissions").canWrite
+const CanRead = require("./Permissions").canRead
 
 module.exports = {
     CheckSetup,
-    SetGeneral
+    SetGeneral,
+    Pagination,
+    CanRemove,
+    CanCreate,
+    CanRead,
+    CanWrite
 }
