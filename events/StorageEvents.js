cd42f9d164f1562de6d35c08d4fc7a4ddf2512be
Tolfx <matteus12122@gmail.com>
2021-02-18 01:28:02 +0100

    New event

---
+++ events/StorageEvents.js
@@ -0,0 +1,11 @@
+const log = require("../lib/Loggers");
+
+module.exports = (storage) => {
+    storage.on("file", (file) => {
+        log.info(`New file was created with name: ` + (file).filename);
+    });
+    
+    storage.on("streamError", (error, conf) => {
+        log.error(error)
+    });
+}
