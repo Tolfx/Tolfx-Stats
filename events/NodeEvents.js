302b2d601082a6d3607841c17aa29524fda28007
Tolfx <matteus12122@gmail.com>
2021-03-07 14:35:56 +0100

    Modified nodevents

--- events/NodeEvents.js
+++ events/NodeEvents.js
@@ -3,14 +3,14 @@ const log = require("../lib/Loggers");
 module.exports = function NodeEventListener()
 {
     process.on('exit', (code) => {
-        log.error(`About to exit with code ${code}`, log.trace());
+        log.error(`About to exit with code ${code}`);
     });
 
     process.on('unhandledRejection', (reason, promise) => {
-        log.error('Unhandled Rejection at: ' + JSON.stringify(promise) + ' reason: ' + reason, log.trace());
+        log.error('Unhandled Rejection at: ' + JSON.stringify(promise) + ' reason: ' + reason);
     });
 
     process.on('uncaughtExceptionMonitor', (err, origin) => {
-        log.error(`An error: ${err}.. at ${origin}`, log.trace());
+        log.error(`An error: ${err}.. at ${origin}`);
     });
 }
